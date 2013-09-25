e01d37a9c3d511850a62aaf29636ace03ea2eb32
laaph <laaph@laaph.com>
2013-09-24 20:11:23 -0700

    Initial Commit

---
+++ Curse/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Curse
+//
+//  Created by stanza on 9/24/13.
+//  Copyright (c) 2013 Laaph. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
